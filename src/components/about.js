9648c00d7d05cb07fd01ac2e26420de12f54e7ac
Jackie Allex <jackieallex@gmail.com>
2022-08-25 21:20:04 -0700

    New demo reel and renders

--- src/components/about.js
+++ src/components/about.js
@@ -11,16 +11,14 @@ function About() {
             <h1>Hello! My name is Jackie, and I'm a creative technologist. </h1>
             </div>
             <h4>I am an undergrad at Northeastern University studying Computer Science and Media Arts.</h4>
-              <h4>A few topics that excite me are computer graphics development, programming artist-facing tools for DCCs, VFX, and game development! In my freetime I 
+              <h4>A few topics that excite me are computer graphics programming, tools for animated film pipelines, and building interactive experiences! In my freetime I 
                 enjoy drawing, playing guitar, and being outdoors.
               </h4>
               <h4> My experience: 
                 <ul>
                 <li><b>Pixar Animation Studios</b> summer 2022 (Software Engineer - Applications intern)</li>
                 <li><b>Disney Research Imagineering</b> spring 2022 (R&D intern)</li>
-                <li><b><a href="https://google.com" target="_blank" rel="noopener noreferrer">
-        Google
-      </a></b>  2022 TL, 2021 SV </li>
+                <li><b>SIGGRAPH</b> 2022 TL, 2021 SV</li>
                 <li><b>Electronic Arts</b> summer 2021 (Tech Art intern)</li>
                 <li><b>ILM, Lucasfilm</b>  spring 2021 (Core Pipeline Software Co-op)</li>
                 <li><b>Human Movement Lab</b> all of 2020 (Motion Capture engineer)</li>
